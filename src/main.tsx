01184b06f5268d57439fbf06f9c338528cc0b9db
Mathis Heder <mathis.heder91@gmail.com>
2025-10-23 21:16:43 +0200

    chore(*): add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route  } from "react-router";
 import './index.css'
 import App from './App.tsx'
+import NotFound from './pages/notFound.tsx';
+import License from './pages/license.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/license" element={<License />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>,
 )
