90501cf6b5df8b16bbd20926dd037fecebd8e1a0
Mathis Heder <mathis.heder91@gmail.com>
2025-10-23 21:27:28 +0200

    chore(*): add shadcn

--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -1,15 +1,25 @@
 import { motion } from "motion/react"
+import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router";
+
 
 export default function NotFound() {
+    const navigate = useNavigate();
     return(
-        <motion.h1
-            className="text-center"
+        <div>
+            <motion.h1
+                className="text-center"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+            >
+                Page has not been foud
 
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.95 }}
-        >
-            Page has not been foud
+            </motion.h1>
 
-        </motion.h1>
+            <div className="flex min-h-svh flex-col items-center justify-center">
+                <Button className="border p-3" onClick={() => navigate("/")}>Click me</Button>
+            </div>
+
+        </div>
     )
 }
